9e2c678cf5cde0bcafa4455de6f551e1c4d8dc33
wanjochan <wanjochan@gmail.com>
2017-12-23 18:01:17 +0800

    M  docs/mg-dtb.js

--- docs/mg-dtb.js
+++ docs/mg-dtb.js
@@ -1,12 +1,11 @@
-//TODO 数据变更时给点效果?
+//TODO 数据变更时给点效果 //watch cellData.lmt .... 然后做个淡入?
 Vue.component('mg-dtb-cell',{
 	props: ['cellData','rowData','headData','tableConf'],
 	template:'<td :rowData="rowData" :width="headData.width" :align="headData.align" :colspan="(tableConf.calcColSpan||function(){})(headData)||headData.colspan" :rowspan="(tableConf.calcRowSpan||function(){})(headData)||headData.rowspan">'
 	+'<template v-if="tableConf.acts&&tableConf.acts[headData.dataIndx]">'
 	+'<template v-for="(act,k) in tableConf.acts[headData.dataIndx]">'
 	+'<template v-if="act.type==\'a\'"><a href="#" :class="act.cls" @click="(act.click||tableConf.rowAction||function(){alert(\'TODO \'+k)})({rowData:rowData,dataIndx:headData.dataIndx,action:k})">{{k}}</a></template>'
-	+'<template v-else="act.type==\'button\'"><button :class="act.cls" v-html="act.html||act.text||k" @click="(act.click||click)({rowData:rowData,dataIndx:headData.dataIndx,action:k})"></button></template>'
-	+'&nbsp;</template></template>'//if acts
+	+'<template v-else="act.type==\'button\'"><button :class="act.cls" v-html="act.html||act.text||k" @click="(act.click||tableConf.rowAction||function(){alert(\'TODO \'+k)})({rowData:rowData,dataIndx:headData.dataIndx,action:k})"></button></template>&nbsp;</template></template>'
 	+'<span v-else v-html="(tableConf.renderCell||function(){})({cellData:cellData,rowData:rowData,dataIndx:headData.dataIndx})||cellData"></span>'
 	+'</td>',
 });
